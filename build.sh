6e984a7dcda0ab8edef84cec9bdb8f8b449b55dd
unknown <nkomarn@hotmail.com>
2019-07-11 23:52:20 -0700

    Some stuff

---
+++ build.sh
@@ -0,0 +1,2 @@
+mvn package -Dbuild.number=14
+yes | cp -rf target/*-*.jar ../Spigot/1.13.2/plugins
---
+++ src/main/java/xyz/nkomarn/SeedTemplate/SeedTemplate.java
@@ -0,0 +1,14 @@
+package xyz.nkomarn.SeedTemplate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SeedTemplate extends JavaPlugin {
+    public void onEnable() {
+        saveDefaultConfig();
+        
+    }
+
+    public void onDisable() {
+        
+    }
+}
